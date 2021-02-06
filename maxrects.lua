96b2099970a601395785ea8a4fa9a9179b82fd56
Muragami <muragami@wishray.com>
2021-02-06 17:24:35 -0500

    initial commit

--- maxrects.lua
+++ maxrects.lua
@@ -1,7 +1,9 @@
--[[
+--[[
 MIT License
 
 Copyright (c) 2021 JasonP
+
+An implementation of MAXRECTS in Lua
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -21,3 +23,58 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 ]]
+
+local function _FindPositionForNewNodeBSSF(width, height, scores)
+end
+
+local function _FindPositionForNewNodeBLSF(width, height, scores)
+end
+
+local function _FindPositionForNewNodeBAF(width, height, scores)
+end
+
+local function _FindPositionForNewNodeBL(width, height, scores)
+end
+
+local function _FindPositionForNewNodeCP(width, height, scores)
+end
+
+-- table for our various heuristic functions
+local FreeRectChoiceHeuristic = {
+		RectBestShortSideFit = _FindPositionForNewNodeBSSF,
+			-- -BSSF: Positions the rectangle against the short side of a free rectangle into which it fits the best.
+		RectBestLongSideFit = _FindPositionForNewNodeBLSF,
+			-- -BLSF: Positions the rectangle against the long side of a free rectangle into which it fits the best.
+		RectBestAreaFit = _FindPositionForNewNodeBAF,
+			-- -BAF: Positions the rectangle into the smallest free rect into which it fits.
+		RectBottomLeftRule = _FindPositionForNewNodeBL,
+			-- -BL: Does the Tetris placement.
+		RectContactPointRule =  _FindPositionForNewNodeCP }
+			-- -CP: Choosest the placement where the rectangle touches other rects as much as possible.
+
+local function _CompareRectShortSide(ra,rb)
+end
+
+local function _NodeSortCmp(ra,rb)
+end
+
+local function _IsContainedIn(ra,rb)
+end
+
+MaxRects = { bWidth = 0, bWeight = 0, bFlip = false,
+	freeRectangles = {}, usedRectangles = {} }
+
+function MaxRects:init(width,height,canflip)
+	self.bFlip = canflip
+	self.bWidth = width
+	self.bHeight = height
+end
+
+function MaxRects:ScoreRect(width, height, method, scores)
+end
+
+function MaxRects:Insert(width, height, method)
+end
+
+function MaxRects:Occupancy()
+end
